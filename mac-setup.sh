be168d805035fd42dff4df158a5863b6b08c1b27
Craig Bruenderman <craig@bruenderman.org>
2019-03-12 15:10:44 -0400

    Bring playbook and vars in

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,10 +2,12 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
-git clone https://github.com/craigbruenderman/mac-setup
+#git clone https://github.com/craigbruenderman/mac-setup
 brew bundle install --file=~/mac-setup/Brewfile
 
 mv ~/mac-setup/.gitconfig ~/
+touch ~/Google-Drive/.ssh-config
+ln -s ~/Google-Drive/.ssh-config ~/.ssh/config
 
 #mkdir ~/.ansible
-#ansible-playbook mac-setup.yml
+ansible-playbook playbook.yml
