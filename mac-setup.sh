2d0cdc8f6481593f6b3ec2661868f1c18be1a133
Craig Bruenderman <craig@bruenderman.org>
2019-03-12 22:28:49 -0400

    Add a bunch of mac preference changes

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install git
 #git clone https://github.com/craigbruenderman/mac-setup
@@ -11,3 +17,9 @@ ln -s ~/Google-Drive/.ssh-config ~/.ssh/config
 
 #mkdir ~/.ansible
 ansible-playbook playbooks/main.yml
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
